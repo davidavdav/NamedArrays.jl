982ce7858aa8dc6034f9e66051a8ae92ec629af3
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-19 00:37:22 +0100

    - display()  - many more indices(), to speed up indexing  - meta-programmed type-deedent getindex() up to three indexes deep  - keep names in slice when last dimesions are singletons  - added names() and dimnames()

--- src/NamedArray.jl
+++ src/NamedArray.jl
@@ -47,7 +47,7 @@ end
 .*(x::NamedArray, y::Number) = *(x,y)
 *(y::Number, x::NamedArray) = *(x,y)
 
-import Base.print, Base.show
+import Base.print, Base.show, Base.display
 print(A::NamedArray) = print(A.array)
 function show(io::IO, A::NamedArray)
     println(io, typeof(A))
@@ -55,6 +55,10 @@ function show(io::IO, A::NamedArray)
     for (n in A.names) print(io, n') end
     println(io, "dimnames: ", A.dimnames')
     println(io, A.array)
+end
+function display(t::TextDisplay, x::NamedArray)
+    display(t, x.names)
+    display(t, x.array)
 end
 
 import Base.size, Base.ndims
@@ -74,17 +78,35 @@ end
 #similar(A::NamedArray, t::DataType) = similar(A, t, size(A.array))
 #similar(A::NamedArray) = similar(A, eltype(A.array), size(A.array))
 
-import Base.getindex, Base.to_index
+import Base.getindex, Base.to_index, Base.indices
 
-## sting indexed arrays, single elements: drop names
-getindex(A::NamedArray, s0::String) = getindex(A, A.dicts[1][s0])
-getindex(A::NamedArray, s::String...) = getindex(A, map(t -> A.dicts[t[1]][t[2]], zip(1:length(s), s))...)
+indices(dict::Dict, I::Real) = I
+indices(dict::Dict, I::Range1) = I
+indices(dict::Dict, I::String) = dict[I]
+indices{T<:String}(dict::Dict, I::Vector{T}) = map(s -> dict[s], I)
 
-## I'm not sure this is the right name...
+## first, we do all combinations of single indices, for efficiency reasons
+getindex(A::NamedArray, i1::Real) = arrayref(A.array,to_index(i1))
+getindex(A::NamedArray, s1::String) = getindex(A, A.dicts[1][s1])
+types = [:Real, :String]
+for T1 in types 
+    for T2 in types
+        @eval getindex(a::NamedArray, i1::$T1, i2::$T2) = getindex(a.array, indices(a.dicts[1], i1), indices(a.dicts[2], i2))
+        for T3 in types 
+            @eval getindex(a::NamedArray, i1::$T1, i2::$T2, i3::$T3) = getindex(a.array, indices(a.dicts[1], i1), indices(a.dicts[2], i2), indices(a.dicts[3], i3))
+            @eval setindex!{T}(a::NamedArray{T}, x, i1::$T1, i2::$T2, i3::$T3) =
+            arrayset(a.array, convert(T,x), indices(a.dicts[1], i1), indices(a.dicts[2], i2), indices(a.dicts[3], i3))
+#            @eval getindex(a::NamedArray, i1::$T1, i2::$T2, i3::$T3, index::Union($(types...))...) = getindex(a.array, indices(a.dicts[1], i1), indices(a.dicts[2], i2), indices(a.dicts[3], i3), map(i -> indices(a.dicts[i], index[i]), 1:length(index))...)
+        end
+    end 
+end
+## This covers everything up over 3 dimensions
+getindex(a::NamedArray, index::Union(Real, String)...) = getindex(a.array, map(i -> indices(a.dicts[i], index[i]), 1:length(index))...)
+
 ## This function takes a range or vector of ints or vector of strings, 
 ## and returns a range or vector of ints, suitable for indexing using traditional 
 ## array indexing
-import Base.indices
+## This makes the general getindex() very slow...
 function indices(dict::Dict, I::IndexOrNamed)
     if isa(I, Real)
         if I<0
@@ -103,7 +125,7 @@ function indices(dict::Dict, I::IndexOrNamed)
             error("Elements of the Names object must be of the same type as the array names for each dimension")
         end
         if I.exclude
-            return map(s -> dict[s], setdiff(k, I.names))
+            return map(s -> dict[s], setdiff(names(dict), I.names))
         else
             return map(s -> dict[s], I.names)
         end
@@ -118,33 +140,35 @@ function indices(dict::Dict, I::IndexOrNamed)
     end
 end
 
-getindex(A::NamedArray, i0::Real) = arrayref(A.array,to_index(i0))
+##getindex(A::NamedArray, i0::Real) = arrayref(A.array,to_index(i0))
 ## for Ranges or Arrays we do an effort keep names
+## We follow the protocol of Array, that the last singleton dimensions are dropped
 function getindex(A::NamedArray, I::IndexOrNamed...)
-    ## This is not completely safe...
-    ## II = map(function(i) indices(A.dicts[i], I[i]) end, 1:length(I))
     II = tuple([indices(A.dicts[i], I[i]) for i=1:length(I)]...)
     a = getindex(A.array, II...)
-    if ndims(a) != ndims(A); return a; end # number of dimension changed
-    @assert ndims(A) == length(II)
-    names = map(i -> getindex(A.names[i],II[i]), 1:length(II))
-    NamedArray{eltype(a),ndims(a)}(a, tuple(names...), tuple(A.dimnames...))
+    dims = map(length, II)
+    n = length(dims)
+    while dims[n]==1 && n>1
+        n -= 1
+    end
+    if ndims(a) != n || length(dims)==1 && ndims(A)>1; return a; end # number of dimension changed
+    names = map(i -> [getindex(A.names[i],II[i])], 1:n)
+    NamedArray{eltype(a),ndims(a)}(a, tuple(names...), tuple(A.dimnames[1:n]...))
 end
 
 ## These seem to be caught by the general getindex, I'm not sure if this is what we want...
-getindex(A::NamedArray, i0::Real) = arrayref(A.array,to_index(i0))
-getindex(A::NamedArray, i0::Real, i1::Real) = arrayref(A.array,to_index(i0),to_index(i1))
-getindex(A::NamedArray, i0::Real, i1::Real, i2::Real) =
-    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2))
-getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real) =
-    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3))
-getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real) =
-    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4))
-getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real, i5::Real) =
-    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4),to_index(i5))
+#getindex(A::NamedArray, i0::Real, i1::Real) = arrayref(A.array,to_index(i0),to_index(i1))
+#getindex(A::NamedArray, i0::Real, i1::Real, i2::Real) =
+#    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2))
+#getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real) =
+#    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3))
+#getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real) =
+#    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4))
+#getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real, i5::Real) =
+#    arrayrefNamed(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4),to_index(i5))
 
-getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real, i5::Real, I::Int...) =
-    arrayref(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4),to_index(i5),I...)
+#getindex(A::NamedArray, i0::Real, i1::Real, i2::Real, i3::Real,  i4::Real, i5::Real, I::Int...) =
+#    arrayref(A.array,to_index(i0),to_index(i1),to_index(i2),to_index(i3),to_index(i4),to_index(i5),I...)
 
 
 import Base.setindex!
@@ -184,6 +208,13 @@ function setindex!(A::NamedArray, x, I::IndexOrNamed...)
     II = tuple([indices(A.dicts[i], I[i]) for i=1:length(I)]...)
     setindex!(A.array, x, II...)
 end
+
+## access to the names of the dimensions
+names(dict::Dict) = collect(keys(dict))[sortperm(collect(values(dict)))] 
+names(a::NamedArray) = map(dict -> names(dict), a.dicts)
+names(a::NamedArray, d::Int) = names(a.dicts[d])
+dimnames(a::NamedArray) = a.dimnames
+dimnames(a::NamedArray, d::Int) = a.dimnames[d]
 
 # this keeps names...
 function hcat{T}(V::NamedVector{T}...) 
