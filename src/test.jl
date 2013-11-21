90fad6c94e93b7b2255c0810645a05d51f915165
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-11-21 10:28:20 +0100

    Some rudimentary testing

--- src/test.jl
+++ src/test.jl
@@ -7,7 +7,7 @@ setnames!(n, ["one", "two"], 1)
 @assert sum(n) == sum(n.array)
 @assert sum(n, 1).array == sum(n.array, 1)
 @assert sum(n, 2).array == sum(n.array, 2)
-@assert sum(n, 2).names[1] == ["one", "two"]
+@assert names(sum(n, 2),1) == ["one", "two"]
 
 ## conversion
 @assert convert(Array, n) == n.array
@@ -17,10 +17,10 @@ first = n.array[1,:]
 @assert n["one", :].array == first
 @assert n[!"two", :].array == first
 @assert n[1, 2:3].array == first[:,2:3]
-@assert n["one", :].names[1] == ["one"]
-@assert n[!"one", :].names[1] == ["two"]
-@assert n[1, !"1"].names[2] == ["2", "3", "4"]
+@assert names(n["one", :],1) == ["one"]
+@assert names(n[!"one", :],1) == ["two"]
+@assert names(n[1, !"1"],2) == ["2", "3", "4"]
 
-## copu
+## copy
 m = copy(n)
 @assert m == n
