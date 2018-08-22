75d0beae43e7d90e360c0aad873d54d3a1d65705
Diego Javier Zea <diegozea@gmail.com>
2018-08-22 21:20:51 +0200

    Use dims correctly, speed up sum

--- src/changingnames.jl
+++ src/changingnames.jl
@@ -13,12 +13,15 @@ for f in (:(Base.sum),
           :(Statistics.mean),
           :(Statistics.std),
           :(Statistics.var))
-    @eval function ($f)(a::NamedArray{T,N}, d::Dims) where {T,N}
-        s = ($f)(a.array, d)
-        dicts = tuple([issubset(i,d) ? OrderedDict(string($f,"(",a.dimnames[i],")") => 1) : a.dicts[i] for i=1:ndims(a)]...)
-        NamedArray{T,N,typeof(a.array), typeof(dicts)}(s, dicts, a.dimnames)
+    @eval function ($f)(a::NamedArray{T,N}; dims=:) where {T,N}
+        s = ($f)(a.array, dims=dims)
+        if !isa(s, AbstractArray)
+            return s
+        else
+            dicts = tuple([issubset(i,dims) ? OrderedDict(string($f,"(",a.dimnames[i],")") => 1) : a.dicts[i] for i=1:ndims(a)]...)
+            NamedArray{T,N,typeof(a.array), typeof(dicts)}(s, dicts, a.dimnames)
+        end
     end
-    @eval ($f)(a::NamedArray, d::Int) = ($f)(a, (d,))
 end
 
 ## I forgot what `fan` stands for.  Function Apply Name?
