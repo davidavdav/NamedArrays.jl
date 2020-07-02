c87e62ae9fe0a53afd1cd5f74b0f9b1901b3417b
Dieter Castel <dietercastel@gmail.com>
2020-07-02 20:22:19 +0200

    Added 4d test.

--- test/write.jl
+++ test/write.jl
@@ -3,9 +3,24 @@ using CSV
 using NamedArrays
 using DataFrames
 
-f = NamedArray([0 1 ; 2 3])
-expected = DataFrame(CSV.File("writetest.csv"))
+include("init-namedarrays.jl")
 
-NamedArrays.write("test.csv",f)
-reread = DataFrame(CSV.File("test.csv"))
-@test reread == expected
+
+@testset "CSV writing" begin
+	expected = DataFrame(CSV.File("CSVs/n2d.csv"))
+	n2d = NamedArray([0 1 ; 2 3])
+	NamedArrays.write("testn2d.csv",n2d)
+	reread = DataFrame(CSV.File("testn2d.csv"))
+	@test reread == expected
+
+	expm4d = DataFrame(CSV.File("CSVs/m4d.csv"))
+	m4d = NamedArray(reshape(1:16,2,2,2,2))
+	setnames!(m4d,["A","B"],1)
+	setnames!(m4d,["x","y"],2)
+	setnames!(m4d,["11","22"],3)
+	setnames!(m4d,["DO","RE"],4)
+
+	NamedArrays.write("testm4d.csv",m4d)
+	rereadm4d = DataFrame(CSV.File("testm4d.csv"))
+	@test rereadm4d == expM 
+end
